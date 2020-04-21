c87ac5f6212489bb045efb326bb23bfa88ee6679
CPPtrueCoder <andierodin@gmail.com>
2020-04-21 23:31:10 +0300

    relized editing of

--- models/course.js
+++ models/course.js
@@ -1,60 +1,84 @@
-const uuid = require ('uuid/v4'),
-	fs = require ('fs'), path = require ('path');
+const uuid = require('uuid/v4');
+const fs = require('fs');
+const path = require('path');
 
 class Course {
 	constructor(title, price, img) {
-		this.title = title;
-		this.price = price;
-		this.img = img;
-		this.id = uuid ();
+		this.title = title
+		this.price = price
+		this.img = img
+		this.id = uuid()
 	}
 	
-	async save() {
-		const courses = await Course.getAllData ();
-		courses.push(this.toJson());
-		return new Promise((resolve,reject)=>{
+	toJSON() {
+		return {
+			title: this.title,
+			price: this.price,
+			img: this.img,
+			id: this.id
+		}
+	}
+	
+	static async update(course) {
+		const courses = await Course.getAll()
+		
+		const idx = courses.findIndex(c => c.id === course.id)
+		courses[idx] = course
+		
+		return new Promise((resolve, reject) => {
 			fs.writeFile(
-				path.join(__dirname,'..','data','courses.json'),JSON.stringify(courses),(err)=>{
-					if (err){
-						reject(err);
-					}
-					else{
-						resolve();
+				path.join(__dirname, '..', 'data', 'courses.json'),
+				JSON.stringify(courses),
+				(err) => {
+					if (err) {
+						reject(err)
+					} else {
+						resolve()
 					}
 				}
 			)
 		})
-	
-	};
-	
-	toJson() {
-		return  {
-				title: this.title,
-				price: this.price,
-				img: this.img,
-				id: this.id
-			}
 	}
 	
-	static getAllData() {
-		return new Promise ((resolve, reject) => {
-			fs.readFile (path.join (__dirname, '..', 'data', 'courses.json'), 'utf-8', (err, content) => {
-				if (err) {
-					reject (err);
+	async save() {
+		const courses = await Course.getAll()
+		courses.push(this.toJSON())
+		
+		return new Promise((resolve, reject) => {
+			fs.writeFile(
+				path.join(__dirname, '..', 'data', 'courses.json'),
+				JSON.stringify(courses),
+				(err) => {
+					if (err) {
+						reject(err)
+					} else {
+						resolve()
+					}
 				}
-				else {
-					resolve (JSON.parse (content));
-					
-				}
-				
-			});
-			
-		});
+			)
+		})
 	}
-	static async getCourse(id){
-	const courses = await 	Course.getAllData();
-	return courses.find(c=>c.id===id);
+	
+	static getAll() {
+		return new Promise((resolve, reject) => {
+			fs.readFile(
+				path.join(__dirname, '..', 'data', 'courses.json'),
+				'utf-8',
+				(err, content) => {
+					if (err) {
+						reject(err)
+					} else {
+						resolve(JSON.parse(content))
+					}
+				}
+			)
+		})
+	}
+	
+	static async getById(id) {
+		const courses = await Course.getAll()
+		return courses.find(c => c.id === id)
 	}
 }
 
-module.exports = Course;
+module.exports = Course
